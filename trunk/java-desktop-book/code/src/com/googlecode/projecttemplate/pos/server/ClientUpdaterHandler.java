07b5823a1f8f41dedc8ff1a23ad5f0d101996e1b
ifnubima <ifnubima@cb4f7db7-ce4a-0410-8ad9-9de50abe1042>
2010-11-22 20:24:20 +0000

    Three tier

---
+++ trunk/java-desktop-book/code/src/com/googlecode/projecttemplate/pos/server/ClientUpdaterHandler.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.googlecode.projecttemplate.pos.server;
+
+/**
+ *
+ * @author ifnu
+ */
+public class ClientUpdaterHandler {
+
+}
